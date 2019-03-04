43873e75db940f0022f59c61fc0d8c2d32861e67
DoloresDai <841856753@qq.com>
2019-03-05 00:48:02 +0800

    Complete all methods of Flaten class.

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,20 +2,37 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> newArrayList = new ArrayList<>();
+        for (Integer[] integers : array) {
+            for (Integer i : integers) {
+                newArrayList.add(i);
+            }
+        }
+        return newArrayList;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> arr = transformToOneDimesional();
+        List<Integer> newArrayList = new ArrayList<>();
+        for (Integer i : arr) {
+            if (!newArrayList.contains(i)) {
+                newArrayList.add(i);
+            }
+        }
+        return newArrayList;
     }
 }
