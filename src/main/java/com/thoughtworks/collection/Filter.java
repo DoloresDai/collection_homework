d6e944e41294692da5f338fd2407a0a11eac0e33
DoloresDai <841856753@qq.com>
2019-03-06 00:46:05 +0800

    Optimizing code with lambda expression.

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -1,8 +1,8 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.util.List;
 
-import java.util.*;
+import static java.util.stream.Collectors.toList;
 
 public class Filter {
 
@@ -13,33 +13,18 @@ public class Filter {
     }
 
     public List<Integer> filterEven() {
-        List<Integer> newArrayList = new ArrayList<>();
-        for (Integer i : array) {
-            if (i % 2 == 0) {
-                newArrayList.add(i);
-            }
-        }
-        return newArrayList;
+        return array.stream().filter(x -> x % 2 == 0).collect(toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
-        List<Integer> newArrayList = new ArrayList<>();
-        for (Integer i : array) {
-            if (i % 3 == 0) {
-                newArrayList.add(i);
-            }
-        }
-        return newArrayList;
+        return array.stream().filter(x -> x % 3 == 0).collect(toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        firstList.retainAll(secondList);
-        return firstList;
+        return firstList.stream().filter(items -> secondList.contains(items)).collect(toList());
     }
 
     public List<Integer> getDifferentElements() {
-        Set<Integer> set = new HashSet<>(array);
-        List<Integer> newArrayList = new ArrayList<>(set);
-        return newArrayList;
+        return array.stream().distinct().collect(toList());
     }
 }
