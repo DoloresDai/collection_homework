9f9566c4396ce0546103bd3c7a468ee34d830f3c
DoloresDai <841856753@qq.com>
2019-03-05 12:40:31 +0800

    Complete all methods of MyMap class.

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,8 +2,19 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.TreeSet;
+
+class Main {
+    public static void main(String[] args) {
+        Integer[] array = new Integer[]{1, 13, 27, 30, 52, 53};
+        List<Integer> list = Arrays.asList(array);
+        MyMap myMap = new MyMap(list);
+        System.out.println(myMap.mapLetters());
+    }
+}
 
 public class MyMap {
 
@@ -17,22 +28,45 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        List<Integer> newArrayList = new ArrayList<>();
+        for (Integer i : array) {
+            newArrayList.add(i * 3);
+        }
+        return newArrayList;
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        List<String> newArrayList = new ArrayList<>();
+        for (Integer i : array) {
+            newArrayList.add(letters[i]);
+        }
+        return newArrayList;
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        List<String> newArrayList = new ArrayList<>();
+        for (Integer i : array) {
+            int firstIndex = (i - 1) / 26;
+            int secondIndex = (i - 1) % 26;
+            if (firstIndex == 0) {
+                newArrayList.add(letters[secondIndex]);
+            } else
+                newArrayList.add(letters[firstIndex - 1] + letters[secondIndex]);
+        }
+        return newArrayList;
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        TreeSet<Integer> treeSet = new TreeSet<>(array);
+        ArrayList<Integer> newArrayList = new ArrayList<>();
+        newArrayList.addAll(treeSet.descendingSet());
+        return newArrayList;
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        TreeSet<Integer> treeSet = new TreeSet<>(array);
+        ArrayList<Integer> newArrayList = new ArrayList<>();
+        newArrayList.addAll(treeSet);
+        return newArrayList;
     }
 }
